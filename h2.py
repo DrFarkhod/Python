f3c69fc5d7292faec9df660bc33367804166c3d8
DrFarkhod <hafizovf@gmail.com>
2022-01-05 15:07:56 +0300

    Стараюсь

---
+++ h2.py
@@ -0,0 +1,5 @@
+#2
+list_number = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+new_list = [list_number[i] for i in range(len(list_number)) if list_number[i-1] < list_number[i]]
+print("Начало списка: " + str(list_number))
+print("Новый список: " + str(new_list))
